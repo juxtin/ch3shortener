bb22c353abd6451e9d5a26d5fb1f44ec7f8c87c2
Justin Holguin <justin.holguin@puppetlabs.com>
2016-02-01 21:48:02 -0800

    wrap-slurp-body middleware

--- src/ch3shortener/middleware.clj
+++ src/ch3shortener/middleware.clj
@@ -1,2 +1,10 @@
-(ns ch3shortener.middleware)
+(ns ch3shortener.middleware
+  (:import [java.io InputStream]))
 
+(defn wrap-slurp-body
+  [handler]
+  (fn [request]
+    (if (instance? InputStream (:body request))
+      (let [prepared-request (update request :body slurp)]
+        (handler prepared-request))
+      (handler request))))
--- test/ch3shortener/middleware_test.clj
+++ test/ch3shortener/middleware_test.clj
@@ -1,4 +1,30 @@
 (ns ch3shortener.middleware-test
   (:require [ch3shortener.middleware :refer :all]
+            [ring.mock.request :as mock]
             [clojure.test :refer :all]))
 
+(deftest wrap-slurp-body-test
+  (let [body "This is a body."
+        request (mock/request :post "/foo" body)
+        expected-request (assoc request :body body)
+        identity-handler (wrap-slurp-body identity)]
+
+    (testing "when given a request with an InputStream body"
+      (let [prepared-request (identity-handler request)]
+        (testing "the body is turned into a string"
+          (is (= body (:body prepared-request)))
+
+          (testing "and the rest of the request is unchanged"
+            (is (= expected-request prepared-request))))))
+
+    (testing "when given a request that has no body"
+      (let [no-body (mock/request :get "/")]
+        (testing "there's no effect"
+          (is (= no-body (identity-handler no-body))))))
+
+    (testing "applying the middleware a second time has no effect"
+      (let [request (mock/request :post "/foo" body)]
+        (is (= expected-request
+               (-> request
+                 identity-handler
+                 identity-handler)))))))
