715983e44558bce4dcf466c1af29459e8f3e9578
Justin Holguin <justin.holguin@puppetlabs.com>
2016-02-01 22:00:45 -0800

    prepare shortener application

--- src/ch3shortener/application.clj
+++ src/ch3shortener/application.clj
@@ -1,6 +1,9 @@
 (ns ch3shortener.application
   (:require [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ch3shortener.storage.in-memory :refer [in-memory-storage]]
             [ch3shortener.routes :as routes]))
 
 (def app
-  (wrap-defaults routes/app-routes api-defaults))
+  (let [stg (in-memory-storage)
+        app-routes (routes/shortener-routes stg)]
+    (wrap-defaults app-routes api-defaults)))
--- src/ch3shortener/routes.clj
+++ src/ch3shortener/routes.clj
@@ -4,6 +4,11 @@
             [compojure.route :as route]
             [ch3shortener.middleware :as mw]))
 
+(defn shortener-routes
+  [stg]
+  (routes
+   (route/not-found "Not Found")))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
