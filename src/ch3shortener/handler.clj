cc62b4e81af3ba7fe4001aa04b6a60dc45dd3a9f
Justin Holguin <justin.holguin@puppetlabs.com>
2016-02-01 19:12:12 -0800

    get-link handler

--- src/ch3shortener/handler.clj
+++ src/ch3shortener/handler.clj
@@ -1,4 +1,10 @@
 (ns ch3shortener.handler
   (:require [ring.util.request :as req]
-            [ring.util.response :as res]))
+            [ring.util.response :as res]
+            [ch3shortener.storage :as st]))
 
+(defn get-link
+  [stg id]
+  (if-let [url (st/get-link stg id)]
+    (res/redirect url)
+    (res/not-found "Sorry, that link doesn't exist.")))
--- test/ch3shortener/handler_test.clj
+++ test/ch3shortener/handler_test.clj
@@ -1,5 +1,26 @@
 (ns ch3shortener.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [ch3shortener.handler :refer :all]))
+            [ch3shortener.handler :refer :all]
+            ;; add the following two requirements
+            [ch3shortener.storage.in-memory :refer [in-memory-storage]]
+            [ch3shortener.storage :as st]))
 
+(deftest get-link-test
+  (let [stg (in-memory-storage)
+        id "test"
+        url "http://test.gov"]
+    ;; store a link directly for the test
+    (st/create-link stg id url)
+
+    (testing "when the ID exists"
+      (let [response (get-link stg id)]
+        (testing "the result is a 302"
+          (is (= 302 (:status response)))
+          (testing "with the expected URL in the Location header"
+            (is (= url (get-in response [:headers "Location"])))))))
+
+    (testing "when the ID does not exist"
+      (let [response (get-link stg "bogus")]
+        (testing "the result is a 404"
+          (is (= 404 (:status response))))))))
